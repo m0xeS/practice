0d0309281e1b95cdde2d58e9b291ffe170a86425
student <student@C251U32>
2020-09-18 11:13:42 +0300

    04_02 completed

---
+++ prac_04_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace prac_04_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue q = new Queue();
+      q.Enqueue("One");
+      q.Enqueue("Two");
+      q.Enqueue("Three");
+      q.Enqueue("Four");
+      q.Enqueue("Five");
+      while (q.Count>0)
+      {
+        object dlt = q.Dequeue();
+        Console.WriteLine("From queue: {0}", dlt);
+      }
+    }
+  }
+}
